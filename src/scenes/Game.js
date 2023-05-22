d987d401a26edc4a5231466455d9cc00619c59ed
Hashim <113859029+Hashxxm@users.noreply.github.com>
2023-05-22 22:14:22 +0300

    refactoring and cleaning

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -24,20 +24,18 @@ export default class Game extends Phaser.Scene
         this.load.image("KitePlace", KitePlace);
         
         this.load.image("Glue", Glue);
-        this.load.image("Rope", Rope);
         this.load.image("Sticks", Sticks);
         this.load.image("KitePaper", KitePaper);
+        this.load.image("Rope", Rope);
 
         this.load.image("GlueTool", GlueTool);
-        this.load.image("RopeTool", RopeTool);
         this.load.image("SticksTool", SticksTool);
         this.load.image("KitePaperTool", KitePaperTool);
+        this.load.image("RopeTool", RopeTool);
     }
 
     create ()
     {
-        this.FoldingOrder = [false, false, false, false];
-
         //Background
         this.add.image(640, 360, 'BG').setScale(0.67);
         this.add.image(640, 360, 'KitePlace').setScale(0.3);
@@ -55,6 +53,10 @@ export default class Game extends Phaser.Scene
         this.KitePaper = this.add.image(640, 360, 'KitePaper').setScale(3);
         this.Rope = this.add.image(732, 425, 'Rope').setScale(0.33);
         
+        //Reference for folding parts and folding parts order
+        this.FoldingParts = [this.Glue, this.Sticks, this.KitePaper, this.Rope];
+        this.FoldingPartsOrder = [false, false, false, false];
+
         //Folding Tools
         this.GlueTool = this.add.image(250, 500, 'GlueTool').setScale(0.33);
         this.GlueTool.setInteractive();
@@ -72,7 +74,7 @@ export default class Game extends Phaser.Scene
         this.RopeTool.setInteractive();
         this.RopeTool.index = 3;
 
-        //Disable Folding parts at beginning
+        //Disable folding parts at beginning
         this.Glue.setVisible(false);
         this.Sticks.setVisible(false);
         this.KitePaper.setVisible(false);
@@ -88,40 +90,12 @@ export default class Game extends Phaser.Scene
 
     ToolClickHandler(pointer, Tool)
     {
-        switch (Tool.index) {
-            case 0:
-                if (this.CheckOrder(0))
-                {
-                    this.Glue.setVisible(true);
-                    this.FoldingOrder[0] = true;
-                }
-                break;
-            case 1:
-                if (this.CheckOrder(1))
-                {
-                    this.Sticks.setVisible(true);
-                    this.FoldingOrder[1] = true;
-                }
-                break;
-            case 2:
-                if (this.CheckOrder(2))
-                {
-                    this.KitePaper.setVisible(true);
-                    this.FoldingOrder[2] = true;
-                }
-                break;
-            case 3:
-                if (this.CheckOrder(3))
-                {
-                    this.Rope.setVisible(true);
-                    this.FoldingOrder[3] = true;
-                }
-                break;
-            default:
-                break;
+        if (this.CheckOrder(Tool.index))
+        {
+            this.FoldingParts[Tool.index].setVisible(true);
+            this.FoldingPartsOrder[Tool.index] = true;
         }
-
-        if (this.CheckOrder(this.FoldingOrder.length))
+        if (this.CheckOrder(this.FoldingPartsOrder.length))
         {
             this.WinText.setVisible(true);
             this.scene.pause()
@@ -132,7 +106,7 @@ export default class Game extends Phaser.Scene
     {
         for (let i = 0; i < index; i++)
         {
-            if (!this.FoldingOrder[i]) return false;
+            if (!this.FoldingPartsOrder[i]) return false;
         }
         return true;
     }
